9e85a314ba62dfc9f44f6411e8d193b8d68498c2
sam boyer <sdboyer@grafana.com>
2021-09-27 22:17:22 -0400

    Commit all generated files

--- public/app/plugins/panel/status-history/models.gen.ts
+++ public/app/plugins/panel/status-history/models.gen.ts
@@ -7,13 +7,22 @@ import * as ui from '@grafana/schema';
 export const modelVersion = Object.freeze([0, 0]);
 
 export interface PanelOptions extends ui.OptionsWithLegend, ui.OptionsWithTooltip {
-  bucketOffset: number;
-  bucketSize?: number;
-  combine?: boolean;
+  alignValue: string;
+  colWidth?: number;
+  rowHeight: number;
+  showValue: ui.VisibilityMode;
 }
 
 export const defaultPanelOptions: PanelOptions = {
-  bucketOffset: 0,
+  alignValue: 'left',
 };
 
-export interface PanelFieldConfig extends ui.GraphFieldConfig {}
+export interface PanelFieldConfig extends ui.HideableFieldConfig {
+  fillOpacity?: number;
+  lineWidth?: number;
+}
+
+export const defaultPanelFieldConfig: PanelFieldConfig = {
+  fillOpacity: 70,
+  lineWidth: 1,
+};
